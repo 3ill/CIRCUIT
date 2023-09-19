caafbe0eb66a385beccfa04616255e6d907baaf4
3illBaby <thrillanon@outlook.com>
2023-09-19 23:32:24 +0100

    updated hardhat config

--- CONTRACT/hardhat.config.js
+++ CONTRACT/hardhat.config.js
@@ -1,15 +1,15 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('solidity-coverage');
 require('dotenv').config();
-const { RPC, KEY } = process.env;
+const { GOERLI_URL, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.19',
   networks: {
     goerli: {
-      url: RPC,
-      accounts: [`0x${KEY}`],
+      url: GOERLI_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
       chainId: 5,
       blockConfirmations: 1,
     },
